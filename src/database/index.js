8aabed43e34f02ecaa31af2ccdc32cedecee524a
Ibukun Chife Didier (Ibk_Akaigen) <adjitchedidier1@gmail.com>
2020-11-09 00:18:38 -0300

    Revert "Backend"

--- src/database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv')
-
-dotenv.config();
-mongoose.connect(`mongodb+srv://appbombeiro:${process.env.PASS}@cluster0.lxrzp.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
